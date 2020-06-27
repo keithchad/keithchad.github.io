550bc1aa5e6f9fbccce3e5aabef3fed12f5aae7f
keithchad <63049827+keithchad@users.noreply.github.com>
2020-06-28 00:17:56 +0300

    inital commit

--- index.html/js/app.js
+++ index.html/js/app.js
@@ -20,3 +20,69 @@ closeBtn.addEventListener("click", function () {
 });
 // set year
 date.innerHTML = new Date().getFullYear();
+
+
+//Firebase Database for storing Contact Info.
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC0XdNnmNnIquzHQ48ejrZW7H0biFrhpVg",
+  authDomain: "my-blog-1b1ba.firebaseapp.com",
+  databaseURL: "https://my-blog-1b1ba.firebaseio.com",
+  projectId: "my-blog-1b1ba",
+  storageBucket: "my-blog-1b1ba.appspot.com",
+  messagingSenderId: "77775088481",
+  appId: "1:77775088481:web:f3212f2bec1b81e538b320",
+  measurementId: "G-KJBLZDMRV1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
