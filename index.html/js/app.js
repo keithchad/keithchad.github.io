2ddedc8a9b3aaba0d0723be9ddf9caff2ff95943
keithchad <63049827+keithchad@users.noreply.github.com>
2020-06-29 00:45:20 +0300

    javascript commit

--- index.html/js/app.js
+++ index.html/js/app.js
@@ -21,6 +21,73 @@ closeBtn.addEventListener("click", function () {
 // set year
 date.innerHTML = new Date().getFullYear();
 
+app.controller('mainCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '$timeout', '$rootScope' , function($scope, $firebaseObject, $firebaseArray, $timeout, $rootScope) {
+
+  $scope.info = {}
+  $scope.info.current_year = new Date().getFullYear();
+  $scope.posts = []
+  var refPosts = firebase.database().ref().child("posts")
+  var temp = $firebaseArray(refPosts.limitToLast(6));
+  temp.$loaded()
+      .then(function() {
+          temp.reverse()
+          angular.forEach(temp, function (post) {
+              var localeDate = post.date
+              var extension = ".png";
+              if (post.image_url.includes(".jpg")){
+                  extension = ".jpg";
+              }else if (post.image_url.includes(".jpeg")){
+                  extension = ".jpeg";
+              }
+              var imageUrl = "https://firebasestorage.googleapis.com/v0/b/my-website-bf8d6.appspot.com/o/blog_images%2F"+localeDate+extension+"?alt=media"
+              post.image = imageUrl;
+              post.date = moment(localeDate, 'YYYYMMDDHHmm').locale('en').format('DD MMMM YYYY')
+          })
+
+          $scope.posts = []
+          for (var i = 0; i < temp.length; i++) {
+              $scope.posts.push(temp[i])
+          }
+          // var old_html = $("#slick").html();
+          // $("#slick").slick('slick');
+          $timeout(function() {
+              $("#slick").slick('unslick')
+              $("#slick").slick({
+                  "slidesToShow": 2,
+                  "slidesToScroll": 2,
+                  "dots": true,
+                  "infinite": false,
+
+                  "responsive": [
+                      {
+                          "breakpoint": 991,
+                          "settings": {
+                              "slidesToShow": 2,
+                              "slidesToScroll": 2
+                          }
+                      },
+                      {
+                          "breakpoint": 575,
+                          "settings": {
+                              "slidesToShow": 1,
+                              "slidesToScroll": 1
+                          }
+                      }
+                  ]
+              });
+          }, 10);
+
+      });
+  $("#role").typed({
+      strings: ["Software Engineer", "Mobile Developer", "Mobile Designer", "Web Developer", "Digital Designer"],
+      typeSpeed: 100,
+      backDelay: 2000,
+      loop: true,
+      loopCount: false,
+      cursorChar: "|",
+  });
+}]);
+
 
 //Firebase Database for storing Contact Info.
 
